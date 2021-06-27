5388b9203c0ed8c1d8b774446686c84894848331
Amoah4m <amoahanthony41@gmail.com>
2021-06-27 23:06:47 +0800

    establisd socketio connection between client and server

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import socketIOClient from "socket.io-client";
 import Home from "./components/Home";
-import LoginBtn from "./components/Auth/login";
+
+//backend server port connector
+const ENDPONT = 'http://127.0.0.1:3001';
 
 function App() {
+    const socket = socketIOClient(ENDPONT);
+    const [message,setmessage] = useState('')
+    const [clientMessage, setClientMessage] = useState('')
+    const [text,setText] = useState('')
+    useEffect(()=>{
+        console.log("here")
+
+        socket.on('node',message=>{
+            setmessage(message)
+        })
+        socket.emit('react','hello from react')
+    });
+
+     const sendmessage = (e)=>{
+         e.preventDefault()
+         socket.emit("react",text);
+         setClientMessage(text);
+     }
   return (
     <div className="App">
         {/*<LoginBtn/>*/}
